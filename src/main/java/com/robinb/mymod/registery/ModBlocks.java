8b09953ac7a2ba6d369ffefe1f5ff39d1df1f5a4
RoBaertschi <rtmbaertschi007@gmail.com>
2021-07-26 10:59:28 +0200

    new

--- src/main/java/com/robinb/mymod/registery/ModBlocks.java
+++ src/main/java/com/robinb/mymod/registery/ModBlocks.java
@@ -1,4 +1,10 @@
 package com.robinb.mymod.registery;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+
 public class ModBlocks {
+
+
+    public static final Block BLOCKOFRUBY =
 }
