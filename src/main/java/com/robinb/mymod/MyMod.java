54010425a4f904a3b58f7a43e6ecd8506e3f1a80
RoBaertschi <rtmbaertschi007@gmail.com>
2021-07-26 14:35:31 +0200

    New Creative Tabs

--- src/main/java/com/robinb/mymod/MyMod.java
+++ src/main/java/com/robinb/mymod/MyMod.java
@@ -4,12 +4,31 @@ import com.robinb.mymod.registery.ModBlocks;
 import com.robinb.mymod.registery.ModItems;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 
 public class MyMod implements ModInitializer {
 	
 	public static final String MOD_ID = "mymod";
 	
-    @Override
+	public static final ItemGroup R_ITEM_GROUP = FabricItemGroupBuilder.build(
+				new Identifier(MOD_ID, "general"),
+				() -> new ItemStack(ModItems.RUBY)
+				);
+	
+	public static final ItemGroup AD_ITEM_G = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "ad_group"))
+			.icon(() -> new ItemStack(Blocks.ENCHANTING_TABLE))
+			.appendItems(stacks -> {
+				stacks.add(new ItemStack(ModBlocks.RUBY_BLOCK));
+				stacks.add(new ItemStack(Items.APPLE));
+				stacks.add(new ItemStack(ModItems.RUBY));
+				stacks.add(new ItemStack(Items.GLOWSTONE_DUST));
+			})
+			.build();
     public void onInitialize() {
     	ModItems.registerItems();
     	ModBlocks.registerBlocks();
--- src/main/java/com/robinb/mymod/registery/ModItems.java
+++ src/main/java/com/robinb/mymod/registery/ModItems.java
@@ -4,17 +4,16 @@ import com.robinb.mymod.MyMod;
 
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 	
 	//Items
-	public static final Item RUBY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+	public static final Item RUBY = new Item(new Item.Settings().group(MyMod.R_ITEM_GROUP));
 	
 	//Block Items
-	public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+	public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(MyMod.R_ITEM_GROUP));
 	
 	public static void registerItems() {
 		Registry.register(Registry.ITEM, new Identifier(MyMod.MOD_ID, "ruby"), RUBY);
